d9b85504855300c837f2c83f3fc18b3adcf19a3d
KowsalyaPraveen <kowsykowsalya24@gmail.com>
2026-02-19 11:08:59 +0530

    Add files via upload
    Added makaia framework

---
+++ makaia_framework-master/src/main/java/com/framework/selenium/api/base/DriverInstance.java
@@ -0,0 +1,95 @@
+package com.framework.selenium.api.base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+
+
+public class DriverInstance  extends AbstractTestNGCucumberTests{
+
+	private static final ThreadLocal<RemoteWebDriver> remoteWebdriver = new ThreadLocal<RemoteWebDriver>();// static WebDriver driver ;
+	// preCondition {driver = new ChromeDriver()}
+	private static final ThreadLocal<WebDriverWait> wait = new  ThreadLocal<WebDriverWait>(); //declaration
+
+	public void setWait() {//setter
+		wait.set(new WebDriverWait(getDriver(), Duration.ofSeconds(10)));// initialization--> new object
+	}
+
+	public WebDriverWait getWait() {//getter
+		return wait.get();
+	}
+
+	public void setDriver(String browser, boolean headless) {
+
+	    switch (browser.toLowerCase()) {
+
+	        case "chrome":
+	            remoteWebdriver.set(new ChromeDriver(getChromeOptions(headless)));
+	            break;
+
+	        case "firefox":
+	            FirefoxOptions firefoxOptions = new FirefoxOptions();
+
+	            if (headless) {
+	                firefoxOptions.addArguments("--headless");
+	            }
+
+	            remoteWebdriver.set(new FirefoxDriver(firefoxOptions));
+	            break;
+
+	        case "edge":
+	            EdgeOptions edgeOptions = new EdgeOptions();
+
+	            if (headless) {
+	                edgeOptions.addArguments("--headless=new");
+	                edgeOptions.addArguments("--window-size=1920,1080");
+	            }
+
+	            remoteWebdriver.set(new EdgeDriver(edgeOptions));
+	            break;
+
+	        default:
+	            System.out.println("Invalid browser. Launching Chrome by default.");
+	            remoteWebdriver.set(new ChromeDriver(getChromeOptions(headless)));
+	    }
+	}
+
+	public RemoteWebDriver  getDriver() {
+		return remoteWebdriver.get();
+	}
+
+	private ChromeOptions getChromeOptions(boolean headless) {
+
+	    ChromeOptions options = new ChromeOptions();
+
+	    options.addArguments("--start-maximized");
+	    options.addArguments("--disable-notifications");
+	    options.addArguments("--disable-infobars");
+	    options.addArguments("--disable-extensions");
+	    options.addArguments("--remote-allow-origins=*");
+	    options.addArguments("--disable-gpu");
+	    options.addArguments("--disable-dev-shm-usage");
+	    options.addArguments("--no-sandbox");
+	    options.addArguments("--user-data-dir=C:\\Users\\DELL\\SleniuProfile");
+	    options.addArguments("--profile-directory=Default");
+
+	    if (headless) {
+	        options.addArguments("--headless=new");
+	        options.addArguments("--window-size=1920,1080");
+	    }
+
+	    return options;
+	}
+
+
+	
+}
